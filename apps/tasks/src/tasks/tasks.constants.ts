73487917535fa7528a7457cfab483a653efd2f51
Santiago Souto <86037575+DevSolto@users.noreply.github.com>
2025-10-05 15:07:06 -0300

    feat(tasks): add service, controller and RMQ events

---
+++ apps/tasks/src/tasks/tasks.constants.ts
@@ -0,0 +1,2 @@
+export const TASKS_EVENTS_CLIENT = 'TASKS_EVENTS_CLIENT';
+export const TASKS_EVENTS_QUEUE = 'tasks.events';
--- apps/tasks/src/tasks/tasks.module.ts
+++ apps/tasks/src/tasks/tasks.module.ts
@@ -1,9 +1,41 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Task } from './task.entity';
+import { TasksService } from './tasks.service';
+import { TasksController } from './tasks.controller';
+import { TASKS_EVENTS_CLIENT, TASKS_EVENTS_QUEUE } from './tasks.constants';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Task])],
-  exports: [TypeOrmModule],
+  imports: [
+    TypeOrmModule.forFeature([Task]),
+    ConfigModule,
+    ClientsModule.registerAsync([
+      {
+        name: TASKS_EVENTS_CLIENT,
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (config: ConfigService) => ({
+          transport: Transport.RMQ,
+          options: {
+            urls: [
+              config.get<string>(
+                'RABBITMQ_URL',
+                'amqp://admin:admin@localhost:5672',
+              ),
+            ],
+            queue: TASKS_EVENTS_QUEUE,
+            queueOptions: {
+              durable: true,
+            },
+          },
+        }),
+      },
+    ]),
+  ],
+  controllers: [TasksController],
+  providers: [TasksService],
+  exports: [TypeOrmModule, TasksService],
 })
 export class TasksModule {}
