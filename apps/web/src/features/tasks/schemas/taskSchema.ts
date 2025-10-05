ef494633cc5d64a617cfac43624fcb0421c30285
Santiago Souto <86037575+DevSolto@users.noreply.github.com>
2025-10-05 16:16:23 -0300

    feat(web): scaffold tasks feature

---
+++ apps/web/src/features/tasks/schemas/taskSchema.ts
@@ -0,0 +1,40 @@
+import { TaskPriority, TaskStatus } from '@contracts'
+import { z } from 'zod'
+
+export const taskStatusSchema = z.nativeEnum(TaskStatus)
+export const taskPrioritySchema = z.nativeEnum(TaskPriority)
+
+export const taskAssigneeSchema = z.object({
+  id: z.string(),
+  username: z.string(),
+})
+
+export const taskSchema = z.object({
+  id: z.string(),
+  title: z.string().min(1),
+  description: z.string().nullish(),
+  status: taskStatusSchema,
+  priority: taskPrioritySchema,
+  dueDate: z.string().nullish(),
+  assignees: z.array(taskAssigneeSchema),
+  createdAt: z.string(),
+  updatedAt: z.string(),
+})
+
+export const createTaskSchema = z.object({
+  title: z.string().min(1),
+  description: z.string().nullish(),
+  status: taskStatusSchema,
+  priority: taskPrioritySchema,
+  dueDate: z.string().nullish(),
+  assignees: z.array(taskAssigneeSchema),
+})
+
+export const updateTaskSchema = createTaskSchema.partial().extend({
+  status: taskStatusSchema.optional(),
+  priority: taskPrioritySchema.optional(),
+})
+
+export type TaskSchema = z.infer<typeof taskSchema>
+export type CreateTaskSchema = z.infer<typeof createTaskSchema>
+export type UpdateTaskSchema = z.infer<typeof updateTaskSchema>
