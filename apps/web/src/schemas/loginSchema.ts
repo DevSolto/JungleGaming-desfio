973a5d5a60bd6336ac5a0d35295ce9373196dc07
Santiago Souto <86037575+DevSolto@users.noreply.github.com>
2025-10-05 11:59:33 -0300

    fix(login): update login schema validation

--- apps/web/src/schemas/loginSchema.ts
+++ apps/web/src/schemas/loginSchema.ts
@@ -1,8 +1,9 @@
-import z from "zod";
+import z from "zod"
 
 export const loginSchema = z.object({
-    email: z.email("Digite um email válido")
-        .min(1, "Informe seu email"),
-    password: z
-        .string({ error: "Digite sua senha" })
+  email: z
+    .string()
+    .email("Digite um email válido")
+    .min(1, "Informe seu email"),
+  password: z.string({ required_error: "Digite sua senha" }),
 })
