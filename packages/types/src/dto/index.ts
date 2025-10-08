a5dc6f12189073765eaa188610a6d1a9ff2d7925
Santiago Souto <86037575+DevSolto@users.noreply.github.com>
2025-10-07 23:12:28 -0300

    fix: align shared type exports with built artifacts

--- packages/types/src/dto/index.ts
+++ packages/types/src/dto/index.ts
@@ -1,7 +1,7 @@
 export * from "./auth.js";
-export * from "./comment.js";
-export * from "./notification.js";
 export * from "./task.js";
-export * from "./tokens.js";
-export * from "./user.js";
-export * from "./http.js";
+export type * from "./comment.js";
+export type * from "./notification.js";
+export type * from "./tokens.js";
+export type * from "./user.js";
+export type * from "./http.js";
