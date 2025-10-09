387e05baad14df5179c7ce0a8668c0c5812183de
santiago <santiagosiqueirasouto@gmail.com>
2025-10-09 04:33:08 -0300

    merge

--- packages/types/dist/contracts/rpc/users.d.ts
+++ packages/types/dist/contracts/rpc/users.d.ts
@@ -1,17 +1,19 @@
 import type { UserDTO, UserListFiltersDTO } from "../../dto/user.js";
 import type { CorrelatedMessage } from "../common/correlation.js";
 export declare const USERS_MESSAGE_PATTERNS: {
-    CREATE: "users.create";
-    FIND_ALL: "users.findAll";
-    FIND_BY_ID: "users.findById";
-    UPDATE: "users.update";
-    REMOVE: "users.remove";
+    readonly CREATE: "users.create";
+    readonly FIND_ALL: "users.findAll";
+    readonly FIND_BY_ID: "users.findById";
+    readonly UPDATE: "users.update";
+    readonly REMOVE: "users.remove";
 };
 export type UsersMessagePatterns = typeof USERS_MESSAGE_PATTERNS;
 export type UsersMessagePattern = UsersMessagePatterns[keyof UsersMessagePatterns];
 export type UsersFindAllPayload = CorrelatedMessage<UserListFiltersDTO>;
 export type UsersFindAllResult = UserDTO[];
-export type UsersFindByIdPayload = CorrelatedMessage<{ id: string }>;
+export type UsersFindByIdPayload = CorrelatedMessage<{
+    id: string;
+}>;
 export type UsersFindByIdResult = UserDTO;
 export interface UsersRpcContractMap {
     [USERS_MESSAGE_PATTERNS.FIND_ALL]: {
@@ -23,3 +25,4 @@ export interface UsersRpcContractMap {
         response: UsersFindByIdResult;
     };
 }
+//# sourceMappingURL=users.d.ts.map
